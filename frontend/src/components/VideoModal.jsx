ec982dc64e3946fc3563d5aece81eef4fdfde21e
charanjegurupati <323103310096@gvpce.in>
2026-06-30 10:54:54 +0530

    Fix ReactPlayer for production

--- frontend/src/components/VideoModal.jsx
+++ frontend/src/components/VideoModal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactPlayer from 'react-player';
+import ReactPlayer from 'react-player/lazy';
 import { IoMdClose } from 'react-icons/io';
 
 const VideoModal = ({ trailerUrl, onClose }) => {
