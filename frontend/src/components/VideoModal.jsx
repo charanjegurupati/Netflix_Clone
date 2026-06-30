5491012f1a682d9076d5571f7b706aaa3be5e73d
charanjegurupati <323103310096@gvpce.in>
2026-06-30 14:19:09 +0530

    Replace react-player with native iframe to fix blank video bugs

--- frontend/src/components/VideoModal.jsx
+++ frontend/src/components/VideoModal.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import ReactPlayer from 'react-player';
 import { IoMdClose } from 'react-icons/io';
 
 const VideoModal = ({ trailerUrl, onClose }) => {
   if (!trailerUrl) return null;
+
+  // Determine if it's a YouTube URL
+  const isYouTube = trailerUrl.includes('youtube.com') || trailerUrl.includes('youtu.be');
+  
+  // Extract YouTube Video ID
+  let youtubeId = '';
+  if (isYouTube) {
+    if (trailerUrl.includes('youtu.be/')) {
+      youtubeId = trailerUrl.split('youtu.be/')[1].split('?')[0];
+    } else if (trailerUrl.includes('watch?v=')) {
+      youtubeId = trailerUrl.split('watch?v=')[1].split('&')[0];
+    }
+  }
 
   return (
     <div className="video-modal-overlay" onClick={onClose}>
@@ -12,14 +24,27 @@ const VideoModal = ({ trailerUrl, onClose }) => {
           <IoMdClose size={30} />
         </button>
         <div className="player-wrapper">
-          <ReactPlayer
-            className="react-player"
-            url={trailerUrl}
-            width="100%"
-            height="100%"
-            playing={true}
-            controls={true}
-          />
+          {isYouTube ? (
+            <iframe
+              className="react-player"
+              src={`https://www.youtube.com/embed/${youtubeId}?autoplay=1&controls=1`}
+              width="100%"
+              height="100%"
+              style={{ border: 'none' }}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              title="Trailer"
+            ></iframe>
+          ) : (
+            <video
+              className="react-player"
+              src={trailerUrl}
+              width="100%"
+              height="100%"
+              controls
+              autoPlay
+            ></video>
+          )}
         </div>
       </div>
     </div>
